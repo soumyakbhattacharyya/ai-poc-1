397905b697aede5ea1c847d14130a2c65a1888f3
soumyak <bhattacharyya.soumyak@gmail.com>
2026-03-17 08:58:15 +0530

    initial commit

---
+++ src/main/java/org/soumyak/ai/ConceptExplainer.java
@@ -0,0 +1,11 @@
+package org.soumyak.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptExplainer {
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptExplainer.class, args);
+	}
+}
---
+++ src/test/java/org/soumyak/ai/AiPoc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soumyak.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPoc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
